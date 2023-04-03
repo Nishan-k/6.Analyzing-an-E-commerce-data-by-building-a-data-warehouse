32fdb68385ece57b48f5a22ba1be69ab710b6bfe
Nishan Karki <karki.nishann@gmail.com>
2023-04-03 05:27:51 +0200

    changes

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ import pandas as pd
 import mysql.connector
 
 df = pd.read_csv("./data/data.csv", encoding='iso-8859-1')
+
 print(df.head())
+
+conn =  mysql.connector.connect(host="127.0.0.1",user="root", password="livelifehard123@")
+
+cursor = conn.cursor()
+cursor.execute("USE ecommerce;")
+
