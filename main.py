3063454f51cee9e67adc07e6bdcb4fe696277033
Nishan Karki <karki.nishann@gmail.com>
2023-04-03 06:05:50 +0200

    CHANGES

--- main.py
+++ main.py
@@ -10,3 +10,49 @@ conn =  mysql.connector.connect(host="127.0.0.1",user="root", password="livelife
 cursor = conn.cursor()
 cursor.execute("USE ecommerce;")
 
+# TODO:1 Create a Invoice_DIM table:
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS INVOICE_DIM(
+        INVOICE_KEY INT NOT NULL AUTO_INCREMENT,
+        INVOICE_NO  VARCHAR(230),
+        PRIMARY KEY(INVOICE_KEY)
+
+    )
+""")
+
+# TODO:2 Create StockCode_DIM table:
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS STOCKCODE_DIM(
+        STOCK_KEY INT NOT NULL AUTO_INCREMENT,
+        STOCKCODE VARCHAR(230),
+        DESCRIPTION VARCHAR(300),
+        PRIMARY KEY(STOCK_KEY)
+    )
+""")
+
+# TODO:3 Create Date_DIM table:
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS DATE_DIM(
+        DATE_KEY INT NOT NULL AUTO_INCREMENT,
+        DATE VARCHAR(30),
+        PRIMARY KEY(DATE_KEY)
+    )
+""")
+
+# TODO:4 Create Customer_DIM table:
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS CUSTOMER_DIM(
+        CUSTOMER_KEY INT NOT NULL AUTO_INCREMENT,
+        CUSTOMER_ID VARCHAR(230),
+        COUNTRY VARCHAR(50),
+        PRIMARY KEY(CUSTOMER_KEY)
+    )
+""")
+
+
+# TODO:5 Create Quantity_Fact table:
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS QUANTITY_FACT(
+        INVOICE_KEY 
+    ) 
+""")
